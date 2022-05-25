07b82e7407e2c2b397c23bc73116bd34198f2686
OlenaLotos <olenameshanych@gmail.com>
2022-05-25 21:52:36 +0300

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,34 @@
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-// const listRef = document.querySelector('.gallery');
-// const itemsMarkup = createGalleryItemMarkup(galleryItems);
+const listRef = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemMarkup(galleryItems);
 
-// listRef.insertAdjacentHTML('afterbegin', itemsMarkup);
+listRef.insertAdjacentHTML('afterbegin', itemsMarkup);
 
-// console.log(createGalleryItemMarkup(galleryItems));
+console.log(createGalleryItemMarkup(galleryItems));
 
-// function createGalleryItemMarkup(galleryItems) {
-//   return galleryItems.map(({preview, original, description}) => {
-//     return `
-//   <a class="gallery__item" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       alt="${description}"
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
       
-//     />
-//   </a>
-// `
-// }) 
-// .join('');
-// }
+    />
+  </a>
+`
+}) 
+.join('');
+}
 
 // // console.log(galleryItems);
 
 // //3, 4//
 
-// new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt"});
+new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt"});
 console.log(galleryItems);
