3703f18854b347c559aac334919cdb6a3ef828bf
OlenaLotos <olenameshanych@gmail.com>
2022-05-26 20:26:10 +0300

    new updating

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,62 +1,76 @@
-import trottle from 'lodash.throttle'
-const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.feedback-form textarea'),
-    input: document.querySelector('.feedback-form input'),
-};
-
-refs.form.addEventListener('submit', onFormSubmit),
-refs.textarea.addEventListener('input', trottle(onTextareaInput, 500));
-// refs.input.addEventListener('input', trottle(onInput, 500));
-
-refs.form.addEventListener('input', event => {
-    // console.log(event.target.name);
-    // console.log(event.target.value);
-
-    formData[event.target.name] = event.target.value;
-    console.log(formData);
-    // console.log( JSON.stringify(formData));
-});
-
-populateTextarea();
-// populateInput();
-
-function onFormSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-    event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
- };
-
-function onTextareaInput(event) { 
-    const message = event.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
-};
-
-function populateTextarea() {
-    // const savedEmail = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-    
-    if (savedMessage) {
-        // console.log(savedMessage);
-        refs.input.value = savedEmail.email;
-        refs.textarea.value = savedEmail.message;           
-    }
-   
-}
-
-// function onInput(event) { 
-//     const email = event.target.value;
-//     localStorage.setItem(STORAGE_KEY, email);
+// import trottle from 'lodash.throttle'
+// const STORAGE_KEY = 'feedback-form-state';
+// const formData = {};
+// const refs = {
+//     form: document.querySelector('.feedback-form'),
+//     textarea: document.querySelector('.feedback-form textarea'),
+//     email: document.querySelector('.feedback-form input'),
 // };
 
-// function populateInput() {
-//     const savedEmail = localStorage.getItem(STORAGE_KEY);
-//     if (savedEmail) {
-//         // console.log(savedEmail);
-//         refs.input.value = savedEmail;
+// savedTextarea();
+// refs.form.addEventListener('submit', onFormSubmit),
+
+// refs.form.addEventListener('input', trottle(event => {
+//     formData[event.target.name] = event.target.value;
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }, 500),
+// );
+
+
+// function onFormSubmit(event) {
+//     event.preventDefault();
+//     console.log(formData);
+//     event.currentTarget.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+//  };
+
+
+// function savedTextarea() {
+//     const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//     if (savedMessage) {
+//         refs.email.value = savedMessage.email;
+//         refs.textarea.value = savedMessage.message;           
 //     }
+   
 // }
+
+import throttle from 'lodash.throttle'
+const STORAGE_KEY = 'feedback-form-state';
+const dataStorage = {};
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+savedTextArea();
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener(
+  'input',
+  throttle(evt => {
+    dataStorage[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataStorage));
+  }, 500),
+);
+// refs.textarea.addEventListener('input', throttle(onMessageInput, 500));
+// function onMessageInput(evt) {
+//   const message = evt.target.value;
+//   //   localStorage.setItem(STORAGE_KEY, message);
+//   //   console.log(message);
+// }
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(dataStorage);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+function savedTextArea() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  //   const parsedMessage = JSON.parse(savedMessage);
+  if (savedMessage) {
+    refs.email.value = savedMessage.email;
+    refs.textarea.value = savedMessage.message;
+  }
+}
+
+
 
